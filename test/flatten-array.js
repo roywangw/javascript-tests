96a920f717480081763a9645e7d7ae0ec258413f
roywangw <ruoyu.wang@marlabsglobal.com>
2014-11-18 14:18:22 -0500

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,7 +2,16 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
- arr = TestApp.Util.flattenAndSort(arr);
+  flattenArray = function (arr) {
+      return flattenInner(arr).sort();
+    },
+
+    flattenInner = function (arr) {
+        arr = Array.prototype.concat.apply([], arr);
+        return arr.some(Array.isArray) ? flattenInner(arr) : arr;
+    };	
+	
+	  arr = flattenArray(arr);   
     expect(arr).toEqual(expected);
   });
 });
