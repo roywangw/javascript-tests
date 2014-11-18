18fa0ac105fd567229d3029d15503843f28cf7ed
roywangw <ruoyu.wang@marlabsglobal.com>
2014-11-18 13:56:16 -0500

    Update clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -2,7 +2,7 @@ describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
-
+   TestApp.Util.copyObject(expected,obj);
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
